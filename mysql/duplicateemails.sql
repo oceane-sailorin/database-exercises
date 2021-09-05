05138702372fb4008d355137de1966d2645c6e21
oceane <oceane.sailorin@live.com>
2021-09-05 21:04:31 +0200

    add duplicate emails

---
+++ mysql/duplicateemails.sql
@@ -0,0 +1,29 @@
+#duplicate emails
+
+SQL Schema
+
+Write a SQL query to find all duplicate emails in a table named Person.
+
++----+---------+
+| Id | Email   |
++----+---------+
+| 1  | a@b.com |
+| 2  | c@d.com |
+| 3  | a@b.com |
++----+---------+
+
+For example, your query should return the following for the above table:
+
++---------+
+| Email   |
++---------+
+| a@b.com |
++---------+
+
+
+# Write your MySQL query statement below
+select Email from Person Group by Email having COUNT(Email) > 1
+
+select p.Email as Email from Person p, Person p2 where p.Email=p2.Email and p.Id != p2.Id group by p.Email
+
+select distinct p.Email as Email from Person p, Person p2 where p.Email=p2.Email and p.Id != p2.Id 
