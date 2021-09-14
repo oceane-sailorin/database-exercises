b0b44455be4bf864e3c86d249df4c718e8436a72
oceane <oceane.sailorin@live.com>
2021-09-14 21:18:19 +0200

    add rising temperature

---
+++ mysql/rising-temperature.sql
@@ -0,0 +1,51 @@
+#rising temperature
+"""
+SQL Schema
+
+Table: Weather
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| recordDate    | date    |
+| temperature   | int     |
++---------------+---------+
+id is the primary key for this table.
+This table contains information about the temperature in a certain day.
+
+ 
+
+Write an SQL query to find all dates' id with higher temperature compared to its previous dates (yesterday).
+
+Return the result table in any order.
+
+The query result format is in the following example:
+
+Weather
++----+------------+-------------+
+| id | recordDate | Temperature |
++----+------------+-------------+
+| 1  | 2015-01-01 | 10          |
+| 2  | 2015-01-02 | 25          |
+| 3  | 2015-01-03 | 20          |
+| 4  | 2015-01-04 | 30          |
++----+------------+-------------+
+
+Result table:
++----+
+| id |
++----+
+| 2  |
+| 4  |
++----+
+In 2015-01-02, temperature was higher than the previous day (10 -> 25).
+In 2015-01-04, temperature was higher than the previous day (20 -> 30).
+"""
+
+
+
+# Write your MySQL query statement below
+select w.id as id from Weather w inner join Weather w2 
+where w2.recordDate = DATE_SUB(recordDate, INTERVAL 1 DAY ) 
+and w.Temperature > w2.Temperature
